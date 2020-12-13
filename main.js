e1c89463c2a92da4c20b5347742a89914a89bbba
Johnny-Oleg <neojin.entity@gmail.com>
2020-12-13 15:54:04 +0300

    final init

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-const MAX_RIVALS = 8,
-    HEIGHT = 100,
-    countSection = Math.floor(document.documentElement.clientHeight / HEIGHT); 
+'use strict';
 
-const $score = document.querySelector('.score'),
-    $start = document.querySelector('.start'),
-    $gameArea = document.querySelector('.game-area'),
-    $car = document.createElement('div'),
-    $audio = document.createElement('embed'),
-    $topScore = document.querySelector('.top-score');
+const MAX_RIVALS = 8;
+const HEIGHT = 100;
+const countSection = Math.floor(document.documentElement.clientHeight / HEIGHT); 
+
+const $score = document.querySelector('.score');
+const $start = document.querySelector('.start');
+const $gameArea = document.querySelector('.game-area');
+const $car = document.createElement('div');
+const $audio = document.createElement('embed');
+const $topScore = document.querySelector('.top-score');
+
+const audio_1 = 'audio/audio_1.mp3';
+const audio_2 = 'audio/audio_2.mp3';
+const audio_3 = 'audio/audio_3.mp3';
+const audio_4 = 'audio/audio_4.mp3';
+const audio_5 = 'audio/audio_5.mp3';
+const audio_6 = 'audio/audio_6.mp3';
+
+const music = [audio_1, audio_2, audio_3, audio_4, audio_5, audio_6];
+const random = arr => arr[Math.floor(Math.random() * arr.length)];
+const randomTrack = random(music);
 
 $car.classList.add('car');
 $gameArea.style.height = countSection * HEIGHT;
-$audio.src = 'audio.mp3';
 $audio.type = 'audio/mp3';
-$audio.style.cssText = `width: 200px; height: 80px; position: absolute; top: 50px; left: 0; z-index: 100`;
+$audio.style.cssText = `
+    width: 200px; height: 80px; position: absolute; top: 50px; left: 0; z-index: 100
+`;
 
 const keys = {
     ArrowUp: false,
     ArrowDown: false,
     ArrowRight: false,
     ArrowLeft: false,
-};
+}
 
 const settings = {
     start: false,
@@ -34,7 +48,7 @@ let level = settings.level;
 
 const getScore = parseInt(localStorage.getItem('score', settings.score));
 
-$topScore.textContent = getScore ? getScore : 0;
+$topScore.textContent = getScore ? `Top Score: ${getScore}` : `Top Score: ${0}`;
 
 const addToLocalStorage = () => {
     if (getScore < settings.score) {
@@ -51,15 +65,15 @@ const startGame = ({ target }) => {
     switch (target.id) {
         case 'easy':
             settings.speed = 3;
-            settings.traffic = 4;
+            settings.traffic = 5;
             break;
         case 'normal':
-            settings.speed = 5;
-            settings.traffic = 3;
+            settings.speed = 4;
+            settings.traffic = 4;
             break;
         case 'hard':
-            settings.speed = 7;
-            settings.traffic = 2;
+            settings.speed = 5;
+            settings.traffic = 3;
             break;
     }
 
@@ -86,15 +100,21 @@ const startGame = ({ target }) => {
         $rival.classList.add('rival');
         $rival.y = rivalSpawn < 100 ? -100 * settings.traffic * (i + 1) : rivalSpawn;
         $rival.style.top = `${$rival.y}px`;
-        $rival.style.background = `transparent url(./image/enemy${randomRival}.png) center / cover no-repeat`;
+        $rival.style.background = `
+            transparent url(./image/enemy${randomRival}.png) center / cover no-repeat
+        `;
 
         $gameArea.append($rival);
         
-        $rival.style.left = `${Math.floor(Math.random() * ($gameArea.offsetWidth - $rival.offsetWidth))}px`;
+        $rival.style.left = `
+            ${Math.floor(Math.random() * ($gameArea.offsetWidth - $rival.offsetWidth))}px
+        `;
     }
 
     settings.score = 0;
     settings.start = true;
+    $audio.src = randomTrack;
+
     $gameArea.append($car);
     document.body.append($audio);
 
@@ -104,12 +124,13 @@ const startGame = ({ target }) => {
 
     settings.x = $car.offsetLeft;
     settings.y = $car.offsetTop;
+    $score.style = `top: 0; left: 0`;
 
     requestAnimationFrame(playGame);
-};    
+}    
 
 const playGame = () => {
-    settings.level = Math.floor(settings.score / 1000);
+    settings.level = Math.floor(settings.score / 3000);
 
     if (settings.level !== level) {
         level = settings.level;
@@ -123,22 +144,28 @@ const playGame = () => {
         moveRoad();
         moveRival();
 
-        (keys.ArrowLeft && settings.x > 0) && (settings.x -= settings.speed);
+        if (keys.ArrowLeft && settings.x > 0) {
+            settings.x -= settings.speed;
+        }
 
-        (keys.ArrowRight && settings.x < ($gameArea.offsetWidth - $car.offsetWidth)) 
-            && (settings.x += settings.speed);
+        if (keys.ArrowRight && settings.x < ($gameArea.offsetWidth - $car.offsetWidth)) {
+            settings.x += settings.speed;
+        }    
 
-        (keys.ArrowUp && settings.y > 0) && (settings.y -= settings.speed);
+        if (keys.ArrowUp && settings.y > 0) {
+            settings.y -= settings.speed;
+        }
 
-        (keys.ArrowDown && settings.y < ($gameArea.offsetHeight - $car.offsetHeight)) 
-            && (settings.y += settings.speed);
+        if (keys.ArrowDown && settings.y < ($gameArea.offsetHeight - $car.offsetHeight)) {
+            settings.y += settings.speed;
+        }
 
         $car.style.left = `${settings.x}px`;
         $car.style.top = `${settings.y}px`;
         
         requestAnimationFrame(playGame);
     }
-};
+}
 
 const startRun = e => {
     if (keys.hasOwnProperty(e.key)) {
@@ -146,7 +173,7 @@ const startRun = e => {
 
         keys[e.key] = true;
     }
-};
+}
  
 const stopRun = e => {
     if (keys.hasOwnProperty(e.key)) {
@@ -154,10 +181,11 @@ const stopRun = e => {
 
         keys[e.key] = false;
     }
-};
+}
 
 const moveRoad = () => {
     const $lines = document.querySelectorAll('.line');
+
     $lines.forEach(line => {
         line.y += settings.speed;
         line.style.top = `${line.y}px`;
@@ -173,30 +201,30 @@ const moveRival = () => {
         let carRect = $car.getBoundingClientRect();
         let rivalRect = rival.getBoundingClientRect();
 
-        if (carRect.top <= rivalRect.bottom 
-            && carRect.right >= rivalRect.left
-            && carRect.left <= rivalRect.right
-            && carRect.bottom >= rivalRect.top
+        if (carRect.top <= rivalRect.bottom && 
+            carRect.right >= rivalRect.left && 
+            carRect.left <= rivalRect.right && 
+            carRect.bottom >= rivalRect.top
         ) {
             $audio.remove();
 
             settings.start = false;
             $start.classList.remove('hide');
             $score.style.top = `${$start.offsetHeight}px`;
+
             addToLocalStorage();
         }
 
         rival.y += settings.speed / 1;
-        rival.style.top = `${rival.y}px`
+        rival.style.top = `${rival.y}px`;
 
         if (rival.y >= $gameArea.offsetHeight) {
-            //?? const checkTop = [...$rivals].every(item => item.offsetTop > HEIGHT);
-
-            //?? checkTop && (rival.y = -HEIGHT * settings.traffic);
-            rival.y = -HEIGHT * settings.traffic //??
-            rival.style.left = `${Math.floor(Math.random() * ($gameArea.offsetWidth - rival.offsetWidth))}px`;
+            rival.y = -HEIGHT * settings.traffic;
+            rival.style.left = `
+                ${Math.floor(Math.random() * ($gameArea.offsetWidth - rival.offsetWidth))}px
+            `;
         }
-    });
+    })
 }
 
 $start.addEventListener('click', startGame);
